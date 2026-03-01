60e473f7c977fd14f355e6ea828bec97eaac3fd2
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2026-03-01 14:39:14 +0100

    add color, typography, and theme definitions for the UI

---
+++ app/src/main/java/com/music42/swiftyprotein/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.music42.swiftyprotein.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFCFBCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF7C4DFF)
+val PurpleGrey40 = Color(0xFF625B71)
+val Pink40 = Color(0xFF7D5260)
+
+val DarkBackground = Color(0xFF1A1B2E)
+val DarkSurface = Color(0xFF232438)
+val DarkSurfaceVariant = Color(0xFF2D2E45)
